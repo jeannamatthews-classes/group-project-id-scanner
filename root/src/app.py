99ecc57d0d67d9a1dc988333205ab947e2207a59
nicksheldon <sheldonnc9@gmail.com>
2025-04-07 22:51:12 -0400

    fixed bugs across backend

--- root/src/app.py
+++ root/src/app.py
@@ -11,11 +11,15 @@ import os  # Used to construct the path to the SQLite file
 from tables import init_db  # Import the database initialization function from models.py
 from config import Config
 
-app = Flask(__name__)  # Creates the Flask application object
-app.config.from_object(Config)
-
 # Configure SQLite database
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Gets the absolute path to the folder
+# I think some of the stuff here belongs in tehe config file
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "../static")
+TEMPLATES_DIR = os.path.join(BASE_DIR, "../templates")
+
+app = Flask(__name__, static_folder=STATIC_DIR, template_folder=TEMPLATES_DIR) # Creates the Flask application object
+
+# Configure database
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"  # Configures database connection
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Disables a feature we don't need
 
@@ -25,7 +29,7 @@ init_db(app)
 # Defines route for homepage (/) that returns a message to confirm the app is running
 @app.route("/")
 def index():
-    return "Database setup complete!"
+    return render_template("index.html")
 
 # Runs Flask development server
 if __name__ == "__main__":
